9674561219328ddcb59645da98d0f4d1b3cc1430
dbruno74 <65651317+dbruno74@users.noreply.github.com>
2021-11-18 12:37:14 +0100

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+use fwupd_dbus::{Client, Signal};
+use std::{
+    error::Error,
+    process,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread,
+};
+
+fn main() {
+    if let Err(why) = main_() {
+        let mut error = format!("error: {}", why);
+        let mut cause = why.source();
+        while let Some(why) = cause {
+            error.push_str(&format!("\n    caused by: {}", why));
+            cause = why.source();
+        }
+
+        eprintln!("{}", error);
+        process::exit(1);
+    }
+}
+
+fn main_() -> Result<(), Box<dyn Error>> {
+    // Atomic value used to stop the background thread.
+    let cancellable = Arc::new(AtomicBool::new(false));
+
+    // Begin listening to signals in the background
+    listen_in_background(cancellable.clone());
+
+    // Create a new dbus client connection.
+    let fwupd = &Client::new()?;
+
+    println!("Version: {}", fwupd.daemon_version()?);
+    println!("Status: {:?}", fwupd.status()?);
+//    println!("Tainted: {}", fwupd.tainted()?);
+   if let Ok(percent) = fwupd.percentage() {
+        println!("Percentage; {}", percent);
+    }
+
+    // Fetch a list of supported devices.
+    for device in fwupd.devices()? {
+        println!("Device: {} {}", device.vendor, device.name);
+
+        if device.is_updateable() {
+            if let Ok(upgrades) = fwupd.upgrades(&device) {
+                println!("  upgrades found");
+                for upgrade in upgrades {
+                    println!("{:#?}", upgrade);
+                }
+            } else {
+                println!("  no updates available");
+            }
+
+            if let Ok(downgrades) = fwupd.downgrades(&device) {
+                println!("  downgrades found");
+                for downgrade in downgrades {
+                    println!("{:#?}", downgrade);
+                }
+            }
+
+            if let Ok(releases) = fwupd.releases(&device) {
+                println!("   releases found");
+                for release in releases {
+                    println!("{:#?}", release);
+                }
+            }
+        } else {
+            println!("  device not updateable");
+        }
+    }
+
+    // Fetch a list of remotes, and update them.
+    for remote in fwupd.remotes()? {
+        println!("{:#?}", remote);
+
+        remote.update_metadata(fwupd)?;
+    }
+
+    // Stop listening to signals in the background.
+    cancellable.store(true, Ordering::SeqCst);
+
+    Ok(())
+}
+
+fn listen_in_background(cancellable: Arc<AtomicBool>) {
+    thread::spawn(move || {
+        if let Ok(fwupd) = Client::new() {
+            // Listen for signals received by the daemon.
+            for signal in fwupd.listen_signals(cancellable) {
+                match signal {
+                    Signal::Changed => {
+                        println!("changed");
+                    }
+                    Signal::DeviceAdded(device) => {
+                        println!("device added: {:?}", device);
+                    }
+                    Signal::DeviceChanged(device) => {
+                        println!("device changed: {:?}", device);
+                    }
+                    Signal::DeviceRemoved(device) => {
+                        println!("device added: {:?}", device);
+                    }
+                    Signal::PropertiesChanged { interface, changed, invalidated } => {
+                        println!(
+                            "Properties of {} changed:\n changed: {:?}\n invalidated: {:?}",
+                            interface, changed, invalidated
+                        );
+                    }
+                }
+            }
+        }
+    });
+}
